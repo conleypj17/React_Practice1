86fa25aeda60c758c8eb2fbe6d799de7e5c960a6
Patrick <conleypj17@gmail.com>
2025-10-18 14:20:16 -0400

    working dropdown options with LA Lakers colors cuz LeBron is the goat

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,19 +2,32 @@
 // multiple selection
 import React, { useState } from 'react';
 import data from './data';
+import './styles.css';
 
 export default function GreatestBasketballPlayer() {
 
     const [selected, setSelected] = useState(null);
+
+    function handleSingleSelection(getCurrentId) {
+        //console.log(getCurrentId)
+        setSelected(getCurrentId === selected ? null : getCurrentId);
+    }
     return (
         <div className="wrapper">
             <div className="GreatestBasketballPlayer">
                 {
                     data && data.length > 0 ? (
                         data.map(dataItem => <div className='item'>
-                            <div className="title">
+                            <div onClick={() => handleSingleSelection(dataItem.id)} className="title">
                                 <h3>{dataItem.question}</h3>
                                 <span>+</span>
+                                <div>
+                                    {selected === dataItem.id ?
+                                        <div className="content">
+                                            {dataItem.answer}
+                                        </div>
+                                        : null}
+                                </div>
 
                             </div>
 
