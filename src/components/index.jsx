49560eed1b6cc769d74d6e60e22cb919bbb81268
Patrick <conleypj17@gmail.com>
2025-10-18 13:52:27 -0400

    Have basic structure displayed on localhost

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,12 +1,29 @@
 // single selection
 // multiple selection
+import React, { useState } from 'react';
+import data from './data';
 
 export default function GreatestBasketballPlayer() {
 
-    const [selected, setSelected] = React.useState(null);
+    const [selected, setSelected] = useState(null);
     return (
-        <div>
-            GreatestBasketballPlayer
+        <div className="wrapper">
+            <div className="GreatestBasketballPlayer">
+                {
+                    data && data.length > 0 ? (
+                        data.map(dataItem => <div className='item'>
+                            <div className="title">
+                                <h3>{dataItem.question}</h3>
+                                <span>+</span>
+
+                            </div>
+
+                        </div>
+                        )
+                    )
+                        : <div>No data available</div>
+                }
+            </div>
         </div>
     );
 }
